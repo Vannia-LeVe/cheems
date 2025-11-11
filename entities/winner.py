3e1d6ccb8cc85485048d268515af391d7fbfc3b0
jramongs100@gmail.com <you@example.com>
2025-11-11 11:29:15 -0700

    winner

--- entities/winner.py
+++ entities/winner.py
@@ -1,5 +1,15 @@
+from persistence.db import get_commection
+
 class Winner:
     def _init_(self,id,name,email):
         self.id=id
         self.name=name
         self.email=email
+
+    def save(self):
+        connection= get_commection ()
+        cursor= connection.cursor()
+
+        #consulta parametrizados
+        query= "INSERT INTO winners(name, email) VALUES(%s, %s)"
+        cursor.execute(query,(self.name, self.email))
